5258ce75791e74b24ecafb186eed86f67f2341af
fangzhijie <fonxian2015@163.com>
2017-06-01 19:18:09 +0800

    添加依赖包

---
+++ web/src/main/java/com/ssm/entity/User.java
@@ -0,0 +1,26 @@
+package com.ssm.entity;
+
+/**
+ * Created by fangzhijie on 2017/6/1.
+ */
+public class User {
+
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
